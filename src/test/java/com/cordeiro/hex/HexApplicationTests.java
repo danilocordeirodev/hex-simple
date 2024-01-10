91ec8c80a51f013332882b19a485956ff8f61275
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2024-01-10 08:58:19 -0300

    initial project

---
+++ src/test/java/com/cordeiro/hex/HexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cordeiro.hex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
