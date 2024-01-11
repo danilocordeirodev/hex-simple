e052ef6f80c583dbc5006c7e7754c834e03eba0d
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2024-01-11 00:58:29 -0300

    add docker and some fixes

--- src/main/java/com/cordeiro/hex/application/core/usecase/DeleteCustomerByIdUseCase.java
+++ src/main/java/com/cordeiro/hex/application/core/usecase/DeleteCustomerByIdUseCase.java
@@ -1,9 +1,10 @@
 package com.cordeiro.hex.application.core.usecase;
 
+import com.cordeiro.hex.application.ports.in.DeleteCustomerByIdInputPort;
 import com.cordeiro.hex.application.ports.in.FindCustomerByIdInputPort;
 import com.cordeiro.hex.application.ports.out.DeleteCustomerByIdOutputPort;
 
-public class DeleteCustomerByIdUseCase {
+public class DeleteCustomerByIdUseCase implements DeleteCustomerByIdInputPort {
 
     private final FindCustomerByIdInputPort findCustomerByIdInputPort;
 
@@ -15,6 +16,7 @@ public class DeleteCustomerByIdUseCase {
         this.deleteCustomerByIdOutputPort = deleteCustomerByIdOutputPort;
     }
 
+    @Override
     public void delete(String id) {
         findCustomerByIdInputPort.find(id);
         deleteCustomerByIdOutputPort.delete(id);
